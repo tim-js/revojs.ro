58666868a4d63828571abf0fa6c72425bd9a3ed7
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-02-02 13:02:58 +0200

    Highlight active page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "gatsby";
 
+import "./Header.css";
+
 export default () => (
   <nav>
     <Link to="/">
       <img src="/logo-revo.js.svg" alt="revo.js" height="50" />
     </Link>
-    <Link to="/contact">contact</Link>
+    <Link activeClassName="active" to="/contact">
+      contact
+    </Link>
   </nav>
 );
