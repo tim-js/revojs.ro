d016ffc80f90905ca11fa3e375991081d8d6fcc7
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-02-02 16:00:58 +0200

    Add PWA support

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,21 @@ module.exports = {
       options: {
         path: `./src/data/`
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "revo.js conference",
+        short_name: "revo.js",
+        start_url: "/",
+        background_color: "#f45919",
+        theme_color: "#f45919",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png" // This path is relative to the root of the site.
+      }
+    },
+    "gatsby-plugin-offline"
   ]
 };
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Helmet from "react-helmet";
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -6,6 +8,13 @@ import "./Layout.css";
 
 export default props => (
   <>
+    <Helmet>
+      <title>revo.js conference</title>
+      <meta
+        name="Description"
+        content="revo.js - Technology Focused. Community Driven. JavaScript Conference."
+      />
+    </Helmet>
     <Header />
     <main role="main">{props.children}</main>
     <Footer />
