8e836e6117cef648068538a2bca0f2993ddb69aa
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-02-03 17:13:27 +0200

    Include global layout styles only once

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
 
-import "./Header.css";
-
 export default () => (
   <nav>
     <Link to="/">
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,8 +4,6 @@ import Helmet from "react-helmet";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-import "./Layout.css";
-
 export default props => (
   <>
     <Helmet>
