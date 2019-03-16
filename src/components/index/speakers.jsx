34c6db6d907de3d3341ad075f71fda7be442bab4
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-03-16 14:53:59 +0200

    Adjust speakers list layout

---
+++ src/components/index/speakers.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { StaticQuery, graphql, Link } from "gatsby";
+
+import Speaker from "../Speaker";
+
+import "./speakers.scss";
+
+const Speakers = ({ data }) => {
+  const { allSpeakersJson, speakerImages } = data;
+  const speakers = allSpeakersJson.edges.map(e => e.node);
+
+  return (
+    <section className="speakers-content">
+      <h1 className="speakers-title">Invited Speakers</h1>
+      <ul className="speakers-list">
+        {speakers.map(speaker => {
+          const speakerImageFluid = speakerImages.edges.find(e =>
+            speaker.image.includes(e.node.base)
+          );
+
+          return (
+            <li key={speaker.id}>
+              <Link to={`/speakers/${speaker.id}`} className="speakers-link">
+                <Speaker data={speaker} image={speakerImageFluid} />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default props => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          allSpeakersJson {
+            edges {
+              node {
+                id
+                firstname
+                lastname
+                image
+                title
+                company
+              }
+            }
+          }
+          speakerImages: allFile(
+            filter: { relativePath: { glob: "speakers/*" } }
+          ) {
+            edges {
+              node {
+                base
+                image: childImageSharp {
+                  fluid(maxWidth: 400, maxHeight: 400, grayscale: true) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => <Speakers data={data} {...props} />}
+    />
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,83 +1,26 @@
 import React from "react";
-import Layout from "../components/Layout";
-import { graphql, Link } from "gatsby";
 
-import Speaker from "../components/Speaker";
+import Layout from "../components/Layout";
+import IndexSpeakers from "../components/index/speakers";
 
 import "./index.scss";
 
-export default props => {
-  const { allSpeakersJson, speakerImages } = props.data;
-  const speakers = allSpeakersJson.edges.map(e => e.node);
-
+export default () => {
   return (
     <Layout>
-      <h1 href="/" className="logo">
-        <img src="logo-revojs.svg" alt="revo.js" height="64" />
-      </h1>
+      <header className="splash">
+        <h2>
+          A two-day JavaScript <strong className="highlight">conference</strong>
+        </h2>
+        <time className="heading" dateTime="2019-10-03">
+          Oct.3-4.2019
+        </time>
+        <span className="location">Timisoara, RO</span>
+      </header>
 
       <main role="main">
-        <header className="splash">
-          <h2>
-            A two-day JavaScript{" "}
-            <strong className="highlight">conference</strong>
-          </h2>
-          <time className="heading" dateTime="2019-10-03">
-            Oct.3-4.2019
-          </time>
-          <span className="location">Timisoara, RO</span>
-        </header>
-
-        <div className="content">
-          <ul className="speakers-list">
-            {speakers.map(speaker => {
-              const speakerImageFluid = speakerImages.edges.find(e =>
-                speaker.image.includes(e.node.base)
-              );
-
-              return (
-                <li key={speaker.id}>
-                  <Link
-                    to={`/speakers/${speaker.id}`}
-                    className="speakers-link"
-                  >
-                    <Speaker data={speaker} image={speakerImageFluid} />
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <IndexSpeakers />
       </main>
     </Layout>
   );
 };
-
-export const pageQuery = graphql`
-  query {
-    allSpeakersJson {
-      edges {
-        node {
-          id
-          firstname
-          lastname
-          image
-          title
-          company
-        }
-      }
-    }
-    speakerImages: allFile(filter: { relativePath: { glob: "speakers/*" } }) {
-      edges {
-        node {
-          base
-          image: childImageSharp {
-            fluid(maxWidth: 400, maxHeight: 400, grayscale: true) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`;
