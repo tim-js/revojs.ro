d175d6dad80707a58fd7571f30d92ba60bffba67
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-03-17 20:52:49 +0200

    Extend Header component to support centering

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import "./header.scss";
 
-export default ({ children, className, ...props }) => (
-  <header className={`page-header ${className}`} {...props}>
-    {children}
-  </header>
-);
+const Header = ({ children, className, centered, ...props }) => {
+  const _centered = centered ? "header--centered" : "";
+
+  return (
+    <header className={`page-header ${_centered} ${className}`} {...props}>
+      {children}
+    </header>
+  );
+};
+
+Header.propTypes = {
+  centered: PropTypes.bool
+};
+
+export default Header;
