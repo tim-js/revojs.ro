ac73122958d9fd2de24567c0d081f963ba9682a8
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-03-18 11:36:09 +0200

    Adjust speaker images + hover effect

--- src/components/Speaker.jsx
+++ src/components/Speaker.jsx
@@ -6,15 +6,16 @@ import "./speaker.scss";
 export default props => {
   return (
     <section className="speaker">
-      <Img
-        fluid={props.image.node.image.fluid}
-        alt=""
-        className="speaker-img"
-      />
+      <figure className="speaker-img">
+        <div className="speaker-img-inner">
+          <Img fluid={props.image.node.image.fluid} alt="" />
+        </div>
+      </figure>
       <div className="speaker-info">
         <div className="speaker-info-inner">
           <h2 className="speaker-name">
-            {props.data.firstname}<br />
+            {props.data.firstname}
+            <br />
             {props.data.lastname}
           </h2>
           <span className="speaker-title">{props.data.title}</span>
