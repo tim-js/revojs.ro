e1009f076ccfdafcd9f6efb1630b842916696a0e
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-03-21 23:22:24 +0200

    Updates animation

--- src/components/index/updates.jsx
+++ src/components/index/updates.jsx
@@ -13,27 +13,17 @@ export default () => {
       <ul className="updates-list">
         <li>
           <Link to="/cfp/">
-            <strong className="mono">Call For Papers</strong>
-            <p className="light-faded">
+            <strong className="update-title mono">Call For Papers</strong>
+            <p className="update-content light-faded">
               Some quick CTA text for papers, bla bla bla lorem ipsum now etc.
             </p>
           </Link>
         </li>
         <li>
           <Link to="/tickets/">
-            <strong className="mono">Tickets</strong>
-            <p className="light-faded">
+            <strong className="update-title mono">Tickets</strong>
+            <p className="update-content light-faded">
               Some quick CTA text for papers, bla bla bla lorem ipsum now etc.
-            </p>
-          </Link>
-        </li>
-        <li>
-          <Link to="/our-mission/">
-            <strong className="mono">
-              Change is the only <span className="highlight">const</span>
-            </strong>
-            <p className="light-faded">
-              Find out why, our mission, bla bla bla lorem ipsum now etc.
             </p>
           </Link>
         </li>
