4e24bd57cb792848d7bd28aa650a2cdfd40413be
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-03-24 16:02:56 +0200

    Re-factor Header component
    - define main/secondary header for pages

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,23 +3,24 @@ import PropTypes from "prop-types";
 
 import "./header.scss";
 
-const Header = ({ children, className, centered, page, ...props }) => {
+const Header = ({ children, className, centered, type, image, ...props }) => {
   const _centered = centered ? "header--centered" : "";
-  const _page = page ? "header--page" : "";
+  const _type = type ? `header--${type}` : "";
+  const _image = image ? image : "";
+  const _className = className || "";
 
   return (
-    <header
-      className={`page-header ${_centered} ${_page} ${className}`}
-      {...props}
-    >
-      {children}
+    <header className={`header ${_centered} ${_type} ${_className}`} {...props}>
+      <div className="header-content">{children}</div>
+      {_image && <div className={`header-image ${_image}`} />}
     </header>
   );
 };
 
 Header.propTypes = {
   centered: PropTypes.bool,
-  page: PropTypes.bool
+  type: PropTypes.oneOf(["main", "secondary"]),
+  image: PropTypes.string
 };
 
 export default Header;
