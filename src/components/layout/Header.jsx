a403b19b164b47550a6191abafc2fa36d85e105b
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-03-24 14:27:18 +0200

    Extend Header component with page variation
    - with smaller height

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,18 +3,23 @@ import PropTypes from "prop-types";
 
 import "./header.scss";
 
-const Header = ({ children, className, centered, ...props }) => {
+const Header = ({ children, className, centered, page, ...props }) => {
   const _centered = centered ? "header--centered" : "";
+  const _page = page ? "header--page" : "";
 
   return (
-    <header className={`page-header ${_centered} ${className}`} {...props}>
+    <header
+      className={`page-header ${_centered} ${_page} ${className}`}
+      {...props}
+    >
       {children}
     </header>
   );
 };
 
 Header.propTypes = {
-  centered: PropTypes.bool
+  centered: PropTypes.bool,
+  page: PropTypes.bool
 };
 
 export default Header;
