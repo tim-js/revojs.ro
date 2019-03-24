b1851dd497e8af9c5d3f6cadf3aee3ad8a4f4510
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-03-24 12:57:41 +0200

    Update index header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,20 +6,40 @@ import IndexUpdates from "@components/index/updates";
 import IndexSpeakers from "@components/index/speakers";
 import IndexSponsors from "@components/index/sponsors";
 import Separator from "@components/Separator";
+import Button from "@components/Button";
 
+import logo from "@assets/logo-revojs.svg";
 import "./index.scss";
 
 export default props => {
   return (
     <Layout location={props.location}>
       <Header className="header-index">
-        <h1>
-          A two-day JavaScript <strong className="highlight">conference</strong>
-        </h1>
-        <time className="mono" dateTime="2019-10-03">
-          Oct.3-4.2019
-        </time>
-        <span className="location">Timisoara, RO</span>
+        <div className="index-logo">
+          <img src={logo} alt="revo.js" height="70" />
+        </div>
+
+        <div className="index-header-content">
+          <h1>
+            Change is the <span className="tagline-block">only const.</span>
+          </h1>
+          <div className="date-location">
+            <time className="index-date mono" dateTime="2019-10-03">
+              <strong>October.3-4.2019</strong>
+            </time>
+            <strong className="index-location mono">Timisoara_RO</strong>
+          </div>
+        </div>
+
+        <div className="index-cta">
+          <Button>View our Speakers</Button>
+        </div>
+
+        {/* <div className="description mono">
+          <strong>Technology focused.</strong>
+          <strong>Community driven.</strong>
+          <strong className="highlight">JavaScript conference.</strong>
+        </div> */}
       </Header>
 
       <IndexUpdates />
