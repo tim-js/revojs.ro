81024f169722771e8af8d8ca1c75d4c3f0cad9ab
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-03-27 15:59:14 +0200

    Update venue content

--- src/pages/venue.jsx
+++ src/pages/venue.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-// import { OutboundLink } from "gatsby-plugin-google-analytics";
+import { OutboundLink } from "gatsby-plugin-google-analytics";
 
 import Layout from "@components/Layout";
 import Header from "@components/layout/Header";
 import Content from "@components/Content";
 import Section from "@components/Section";
 import Note from "@components/Note";
+import Button from "@components/Button";
 
 export default () => {
   return (
@@ -17,12 +18,26 @@ export default () => {
       <Section light>
         <Content centered>
           <Note>
-            <p>Details about the location will be published soon.</p>
+            <p style={{ textAlign: "center" }}>
+              revo.js will take place in the Multifunctional Hall of our County
+              Council. <br />
+              <strong>Address</strong>: Bulevardul Revoluției din 1989, nr. 17
+            </p>
             <br />
-            <br />
-            <br />
+            <p style={{ textAlign: "center" }}>
+              More information will be available soon.
+            </p>
             <br />
           </Note>
+
+          <p style={{ textAlign: "center" }}>
+            <OutboundLink
+              href="http://maps.google.com/maps?q=45.7562%2C21.2371+%28Consiliul+Judetean+Timis%2C+Bulevardul+Revolu%C8%9Biei+din+1989+nr.+17%2C+Timi%C8%99oara+300034%29"
+              target="_blank"
+            >
+              <Button white>View location on map</Button>
+            </OutboundLink>
+          </p>
         </Content>
       </Section>
     </Layout>
