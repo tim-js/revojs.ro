2778b39dc8d25c57b8cabc8fa87882df2b5ed608
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-03-27 18:20:15 +0200

    Re-factor Subscribe component to work without JS

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -3,16 +3,24 @@ import PropTypes from "prop-types";
 
 import "./subscribe.scss";
 
+const listIds = Object.freeze({
+  cfp: "57960db8f5",
+  default: "ec632308b8"
+});
+
 const Subscribe = props => {
   const [email, setEmail] = useState("");
 
   const _theme = props.light ? "subscribe-form--light" : "";
   const _centered = props.centered ? "subscribe-form--centered" : "";
 
+  const listId = listIds[props.list] || listIds.default;
+
   return (
     <form
-      onSubmit={() => openSubscribePage(email, props.list)}
+      action="https://revojs.us20.list-manage.com/subscribe"
       className={`subscribe-form ${_theme} ${_centered}`}
+      method="GET"
     >
       <input
         className="subscribe-input"
@@ -21,7 +29,10 @@ const Subscribe = props => {
         value={email}
         onChange={e => setEmail(e.target.value)}
         id="subscribe-email"
+        name="EMAIL"
       />
+      <input type="hidden" name="u" value="34c79aeaaf8573f6289982fdd" />
+      <input type="hidden" name="id" value={listId} />
       <button className="subscribe-button" type="submit">
         <span className="subscribe-arrow">Subscribe</span>
       </button>
@@ -40,15 +51,3 @@ Subscribe.defaultProps = {
 };
 
 export default Subscribe;
-
-const listIds = Object.freeze({
-  cfp: "57960db8f5",
-  default: "ec632308b8"
-});
-
-function openSubscribePage(email, list) {
-  const listId = listIds[list] || listIds.default;
-  window.open(
-    `https://revojs.us20.list-manage.com/subscribe?u=34c79aeaaf8573f6289982fdd&id=${listId}&EMAIL=${email}`
-  );
-}
