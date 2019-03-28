d5deb6fab408ea96a73bcaf4a6eefee16e8550a6
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-03-28 15:59:17 +0200

    Enhance Layout component to be able to pass page title

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,21 +4,24 @@ import Helmet from "react-helmet";
 import Navigation from "@components/Navigation";
 import Footer from "@components/Footer";
 
-export default ({ location, children, ...props }) => (
-  <>
-    <Helmet>
-      <title>revo.js conference</title>
-      <meta
-        name="Description"
-        content="revo.js - Technology Focused. Community Driven. JavaScript Conference."
-      />
-    </Helmet>
-    <Navigation location={location} />
+export default ({ location, children, title, ...props }) => {
+  const _title = title || "revo.js conference";
+  return (
+    <>
+      <Helmet>
+        <title>{_title}</title>
+        <meta
+          name="Description"
+          content="revo.js - Technology Focused. Community Driven. JavaScript Conference."
+        />
+      </Helmet>
+      <Navigation location={location} />
 
-    <main role="main" {...props}>
-      {children}
-    </main>
+      <main role="main" {...props}>
+        {children}
+      </main>
 
-    <Footer />
-  </>
-);
+      <Footer />
+    </>
+  );
+};
