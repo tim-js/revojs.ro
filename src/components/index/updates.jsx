237df7e8afd33b03e4418318ff60f269a1f6a42f
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-07-06 14:56:34 +0300

    Updates index updates

--- src/components/index/updates.jsx
+++ src/components/index/updates.jsx
@@ -12,16 +12,19 @@ export default () => {
 
       <ul className="updates-list">
         <li>
-          <Link to="/cfp/">
-            <strong className="update-title mono">Call For Papers has Closed</strong>
+          <Link to="/workshops/#efficient-end-2-end-testing-with-cypress-io">
+            <strong className="update-title mono">Workshop available</strong>
             <p className="update-content light-faded">
-              CFP was open between 17 Apr - 17 Jun. We will annouce the selected speakers around Mid July.
+              Join Gleb's workshop on October 2nd and learn Efficient End-to-End
+              Testing with Cypress.io.
             </p>
           </Link>
         </li>
         <li>
           <Link to="/tickets/">
-            <strong className="update-title mono">Regular Tickets On Sale</strong>
+            <strong className="update-title mono">
+              Regular Tickets On Sale
+            </strong>
             <p className="update-content light-faded">
               The 50 Early Birds flew away. Regular tickets are now available.
             </p>
