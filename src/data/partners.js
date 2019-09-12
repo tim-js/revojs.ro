16aee55e725ad9a80784564c0ef10ce4bf1f821c
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-09-12 10:48:29 +0300

    Add partner logo

--- src/data/partners.js
+++ src/data/partners.js
@@ -2,26 +2,31 @@ export const partners = [
   {
     name: "Bereta",
     image: "partners/bereta_logo.svg",
-    url: "https://beretatm.ro"
+    url: "https://beretatm.ro",
   },
   {
     name: "Progress",
     image: "partners/progress_logo.svg",
-    url: "https://www.progress.com/"
+    url: "https://www.progress.com/",
   },
   {
     name: "Auth0",
     image: "partners/Auth0_logo.svg",
-    url: "https://auth0.com/"
+    url: "https://auth0.com/",
   },
   {
     name: "DevPlant",
     image: "partners/DevPlant_logo.svg",
-    url: "https://devplant.ro/"
+    url: "https://devplant.ro/",
   },
   {
     name: "Microsoft",
     image: "partners/microsoft_logo.svg",
-    url: "https://www.microsoft.com"
-  }
+    url: "https://www.microsoft.com",
+  },
+  {
+    name: "CRAFTWAND",
+    image: "partners/craftwand_logo.svg",
+    url: "https://www.craftwand.info/",
+  },
 ];
