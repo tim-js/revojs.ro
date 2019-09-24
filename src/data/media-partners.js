347752907c6756b2e09714b01a8792282777821b
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-09-24 07:20:55 +0300

    Remove Hackages and CTRL-D

--- src/data/media-partners.js
+++ src/data/media-partners.js
@@ -1,27 +1,27 @@
 export const media = [
-  {
-    name: "CTRL-D",
-    image: "partners/ctrl-D_logo.svg",
-    url: "https://ctrl-d.ro"
-  },
+  // {
+  //   name: "CTRL-D",
+  //   image: "partners/ctrl-D_logo.svg",
+  //   url: "https://ctrl-d.ro"
+  // },
   {
     name: "Timisoara.Tech",
     image: "partners/TimisoaraTech_logo@2x.png",
-    url: "https://timisoara.tech/"
+    url: "https://timisoara.tech/",
   },
   {
     name: "React GraphQL Academy",
     image: "communities/ReactGraphQL-Academy_logo@2x.png",
-    url: "https://reactgraphql.academy/"
+    url: "https://reactgraphql.academy/",
   },
-  {
-    name: "Hackages",
-    image: "communities/hackages_logo@2x.png",
-    url: "https://hackages.io/"
-  },
+  // {
+  //   name: "Hackages",
+  //   image: "communities/hackages_logo@2x.png",
+  //   url: "https://hackages.io/"
+  // },
   {
     name: "techevents.online",
     image: "partners/TechEvents_logo@2x.png",
-    url: "https://www.techevents.online/"
-  }
+    url: "https://www.techevents.online/",
+  },
 ];
