0395026dd9176d6e04c3c8b99439393548419c6d
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-10-31 08:24:45 +0200

    Add separation for organizers

--- src/pages/2019/organizers.jsx
+++ src/pages/2019/organizers.jsx
@@ -40,7 +40,7 @@ const Organizers = ({ data }) => {
       <Section light>
         <Content centered>
           <section>
-            <h2>Main Organizers</h2>
+            <h1>Main Organizers</h1>
 
             <div className="organizers">
               <section>
@@ -96,31 +96,29 @@ const Organizers = ({ data }) => {
         </Content>
       </Section>
 
-      <div className="footer-separation">
-        <Section>
-          <section className="organizers-team">
-            <h2>Core Team</h2>
-            <p>
-              This event takes place only because the following humans got
-              involved:
-            </p>
-            <ul className="team-list">
-              {team.map(member => (
-                <li className="team-member" key={member.name}>
-                  <Img
-                    fluid={member.memberImage.node.image.fluid}
-                    alt={`${member.name} black and white photo`}
-                  />
-                  <strong className="team-member-name">
-                    {member.name}
-                    <span className="team-member-title">{member.title}</span>
-                  </strong>
-                </li>
-              ))}
-            </ul>
-          </section>
-        </Section>
-      </div>
+      <Section className="organizers-team">
+        <section>
+          <h1>Core Team</h1>
+          <p>
+            This event takes place only because the following humans got
+            involved:
+          </p>
+          <ul className="team-list">
+            {team.map(member => (
+              <li className="team-member" key={member.name}>
+                <Img
+                  fluid={member.memberImage.node.image.fluid}
+                  alt={`${member.name} black and white photo`}
+                />
+                <strong className="team-member-name">
+                  {member.name}
+                  <span className="team-member-title">{member.title}</span>
+                </strong>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </Section>
     </Layout>
   );
 };
