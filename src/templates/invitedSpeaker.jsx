760bdb2b81a5001115367b7e874848f5fd4d732d
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-11-17 18:59:44 +0200

    Fix linting errors

--- src/templates/invitedSpeaker.jsx
+++ src/templates/invitedSpeaker.jsx
@@ -47,9 +47,9 @@ export default props => {
             width="100%"
             height="400"
             src="https://www.youtube.com/embed/KY9ZVsGnTes"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen="true"
+            allowFullScreen={true}
           ></iframe>
         </Content>
       </Section>
