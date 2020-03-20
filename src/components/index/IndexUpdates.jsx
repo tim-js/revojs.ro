3e7fca1f33c8d3fb8843ad81fcd3f0d67ba74d8e
Andrei Pfeiffer <andrei@upsidedown.ro>
2020-03-20 14:27:41 +0200

    Index updates

--- src/components/index/IndexUpdates.jsx
+++ src/components/index/IndexUpdates.jsx
@@ -11,13 +11,27 @@ export default () => {
     <Section centered>
       <h2 className="big first">Updates</h2>
 
+      <br />
+
       <ul className="updates-list">
+        <li>
+          <Link to="/covid19/" className="updates-link">
+            <strong className="update-title mono">COVID-19 Updates</strong>
+            <p className="update-content light-faded">
+              Follow the latest revo.news on how the current pandemic influences
+              our schedule.
+            </p>
+            <div className="arrow">
+              <Arrow color="accent" />
+            </div>
+          </Link>
+        </li>
         <li>
           <Link to="/2020/tickets/" className="updates-link">
             <strong className="update-title mono">Tickets</strong>
             <p className="update-content light-faded">
-              Discover what type of ticket suits you best and grab it! Early
-              Bird starts at the end of March.
+              The launch of Early Bird tickets is slightly postponed to May 1st.
+              Stay tuned.
             </p>
             <div className="arrow">
               <Arrow color="accent" />
@@ -28,8 +42,8 @@ export default () => {
           <Link to="/2020/cfp/" className="updates-link">
             <strong className="update-title mono">Call for Papers</strong>
             <p className="update-content light-faded">
-              CFP opens on April 1st. No foolin' around, we promise. We'd love
-              to see your proposal.
+              CFP continues as planned and opens on April 1st. We’re not fooling
+              around.
             </p>
             <div className="arrow">
               <Arrow color="accent" />
