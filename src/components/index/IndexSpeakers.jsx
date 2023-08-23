f2f0e42aa45f15f42c9562ca4ee7a5ed16ed9749
Adrian Fâciu <2067101+adrianfaciu@users.noreply.github.com>
2023-08-23 17:34:10 +0300

    Increase speaker image size on home

--- src/components/index/IndexSpeakers.jsx
+++ src/components/index/IndexSpeakers.jsx
@@ -77,8 +77,8 @@ const IndexSpeakers = (props) => {
             base
             childImageSharp {
               gatsbyImageData(
-                width: 400
-                height: 400
+                width: 600
+                height: 600
                 transformOptions: { grayscale: false }
                 layout: CONSTRAINED
               )
